ea494837df472bc5ca02848ecfc05b902e5743de
belowsurface3000 <f.schoeneberger@gmx.net>
2023-06-07 07:07:37 +0200

    change scss to angular way

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -5,6 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
+
 export class NavigationComponent {
 
+  showNavigation = false;
+
+  triggerNavigation() {
+    console.log("click")
+  }
+
+  toggleNavigation() {
+    this.showNavigation = !this.showNavigation;
+  }
 }
