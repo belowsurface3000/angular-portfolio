b6c9b44125941f71071d6f61f4bcad87e0a8fbc4
belowsurface3000 <f.schoeneberger@gmx.net>
2023-06-07 09:41:46 +0200

    nav button scroll effect is working

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Renderer2, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -10,6 +10,51 @@ export class NavigationComponent {
 
   navigation = false;
   navigationHidden = true;
+  scrollUp: boolean;
+  currentScrollPos: number;
+  prevScrollPos: number;
+
+  constructor(private renderer: Renderer2) {
+    this.scrollUp = false;
+    this.navigationHidden = false;
+    this.currentScrollPos = 0;
+    this.prevScrollPos = 0;
+  }
+
+  ngOnInit() {
+    console.log(this.navigationHidden)
+    // Check the initial window size
+    if (window.innerWidth <= 1500) {
+      this.navigationHidden = true;
+    }
+
+    // Call the scroll event handler
+    this.onScroll();
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    this.currentScrollPos = window.pageYOffset;
+    
+    if (this.currentScrollPos < window.innerHeight / 2) {
+      this.scrollUp = true;
+    } else if (this.prevScrollPos > this.currentScrollPos) {
+      this.scrollUp = true;
+    } else {
+      this.scrollUp = false;
+    }
+
+    this.prevScrollPos = this.currentScrollPos;
+  }
+
+  @HostListener('window:resize', [])
+  onResize(): void {
+    if (window.innerWidth <= 1500) {
+      this.navigationHidden = true;
+    } else {
+      this.navigationHidden = false;
+    }
+  }
 
   triggerNavigation() {
     this.navigation = !this.navigation;
