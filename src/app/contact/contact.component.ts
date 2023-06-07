feec8ce10e580430d1f2a4cab3850cf781145d3a
belowsurface3000 <f.schoeneberger@gmx.net>
2023-06-07 19:10:07 +0200

    contact form working

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from '@emailjs/browser';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-contact',
@@ -17,10 +19,19 @@ export class ContactComponent {
     this.showContactForm = false;
   }
 
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    emailjs.sendForm(environment.EMAIL_SERVICE, environment.EMAIL_TEMPLATE, e.target as HTMLFormElement, environment.EMAIL_USER)
+      .then((result: EmailJSResponseStatus) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+
   updateUserInput(e: Event) {
     const target = e.target as HTMLInputElement;
     if (target.value != "okay") return;
     this.showContactForm = true;
   }
-
 }
